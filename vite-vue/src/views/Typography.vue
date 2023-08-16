516ae7f38223c6d8f796b504191d2bbd017a0696
Nick <kalunay@gmail.com>
2023-08-16 17:32:00 +0300

    first commit

---
+++ vite-vue/src/views/Typography.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1 class="heading-1">Typography</h1>
+</template>
+
+<style lang="scss">
+.heading {
+    &-1 {
+        font-size: 34px;
+        margin: 20px 0;
+        font-weight: 300;
+    }
+}
+</style>
