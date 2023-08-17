4f8e10dc40ae127100e4c032705af680dc3cc566
Nick <kalunay@gmail.com>
2023-08-17 17:50:34 +0300

    add progress

---
+++ vite-vue/src/views/Progress.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue';
+import ProgressBar from '@/components/Progress/ProgressBar.vue';
+import ProgressCircle from '@/components/Progress/ProgressCircle.vue';
+import Button from '@/components/Button.vue'
+
+const percentBar = ref(40);
+const percentCircle = ref(60);
+const addPercentBar = () => {
+    percentBar.value += 10;
+    if(percentBar.value > 100){
+        percentBar.value = 100;
+    }
+}
+</script>
+
+<template>
+    <h1 class="heading-1">Progress</h1>
+    <progress-bar :percent="percentBar" color="success" />
+    <Button label="Add" @click="addPercentBar" />
+    <ProgressCircle :percent="percentCircle" color="warning" />
+</template>
