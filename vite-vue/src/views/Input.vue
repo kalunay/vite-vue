6414f7563421e570a4356598d6086a0f4547db75
Nick <kalunay@gmail.com>
2023-08-18 12:29:36 +0300

    add input

---
+++ vite-vue/src/views/Input.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue';
+
+import Input from '@/components/Input.vue';
+import Button from '@/components/Button.vue';
+
+const nameField = ref('');
+const submitForm = () => {
+    
+}
+</script>
+
+<template>
+    <h1 class="heading-1">Inputs</h1>
+    <form @submit.prevent="submitForm">
+        <Input 
+            label="Your name"
+            name="name"
+            placeholder="Input your name"
+            v-model:value="nameField"
+        />
+
+        <Button label="Submit" color="primary"></Button>
+    </form>
+</template>    
